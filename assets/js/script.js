8e159532723f819d6cdf1b7333f376cb8e3cb8af
vlsulliv <vlsulliv@yahoo.com>
2022-02-10 03:06:40 -0600

    align form and add js for functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,56 @@
-let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let lower = "abcdefghijklmnopqrstuvwxyz"
-let special = "!@#$%^&*()_-<>>?/]["
+const charecterRange = document.getElementById('passwordrange');
+const passwordLength = document.getElementById('passwordlength');
+const addUpperCase = document.getElementById('upperCase');
+const addLowerCase = document.getElementById('lowerCase');
+const addSymbols = document.getElementById('symbols');
+const form = document.getElementById('form');
+const displayPassword = document.getElementById('passwordtext');
 
-let password = [];
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
 
-let getRandNum = function(){
-  let getRandNum = Math.floor(10 * Math.random());
-  console.log(getRandNum);
-  console.log(typeof getRandNum);
-  return;
+form.addEventListener('submit', e=> {
+  e.preventDefault()
+  const charecterAmount = passwordLength.value
+  const includeUpperCase = addUpperCase.checked
+  const includeLowerCase = addLowerCase.checked
+  const includeSymbols = addSymbols.checked
+  const password = generatePassword(charecterAmount, includeUpperCase, includeSymbols)
+  displayPassword.innerText = password
+})
+
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
 }
 
-getRandNum();
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
 
-console.log('')
-// console.log(floor.random()*65)
-// console.log(Math.ceil());
-
-// alert("wtf do you want")
-// function basePassword() {
-// for (let i=0; i<9; i++) {
-//   let randNum = Math.floor(Math.random()*10);
-//   console.log(randNum);
-//   password.push(randNum);
-//   console.log(password);
-//   }
-// }
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
