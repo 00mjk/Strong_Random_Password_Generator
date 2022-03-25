775a9bfa76b0606139b1e63c5896914841ab6131
vlsulliv <vlsulliv@yahoo.com>
2022-03-25 00:29:34 -0500

    Add favicon and JS logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,81 @@
-const charecterRange = document.getElementById('passwordrange');
-const passwordLength = document.getElementById('passwordlength');
-const addUpperCase = document.getElementById('upperCase');
-const addLowerCase = document.getElementById('lowerCase');
-const addSymbols = document.getElementById('symbols');
-const form = document.getElementById('form');
-const displayPassword = document.getElementById('passwordtext');
+let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let symbols = ["~","!","@","#","$","%","^","&","*","(",")","_","+","}","{",":","<",">","?","}"];
+let numbers = ["0","1","2","3","4","5","6","7","8","9"];
 
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
-  arrayFromLowToHigh(58, 64)
-).concat(
-  arrayFromLowToHigh(91, 96)
-).concat(
-  arrayFromLowToHigh(123, 126)
-)
+let pw = [];
 
-form.addEventListener('submit', e=> {
-  e.preventDefault()
-  const charecterAmount = passwordLength.value
-  const includeUpperCase = addUpperCase.checked
-  const includeLowerCase = addLowerCase.checked
-  const includeSymbols = addSymbols.checked
-  const password = generatePassword(charecterAmount, includeUpperCase, includeSymbols)
-  displayPassword.innerText = password
-})
+let password = document.getElementById('password').value;
+let pwText = document.getElementById('passwordtext');
+HTMLElement.addQuerySelectorAll();
+// let ibutton = document.getElementById('initButton').addQuerySelector();
 
-function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
-  let charCodes = LOWERCASE_CHAR_CODES
-  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
-  
-  const passwordCharacters = []
-  for (let i = 0; i < characterAmount; i++) {
-    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
-    passwordCharacters.push(String.fromCharCode(characterCode))
+console.log(pwText)
+console.log("ibutton");
+console.log(typeof(initButton));
+
+document.addEventListener('click', function(e) {
+  e.preventDefault();
+  requestUserInput(); 
+  pwText.textContent = pw;
   }
-  return passwordCharacters.join('')
-}
+) 
 
-function arrayFromLowToHigh(low, high) {
-  const array = []
-  for (let i = low; i <= high; i++) {
-    array.push(i)
+
+
+function requestUserInput() {
+
+  var length = prompt("How many charecters would you like your password to have?");
+
+  if (isNaN(length) === true) {
+    alert("please enter a valie number");
+    return;
+  } 
+
+  if (length < 8 || length > 128) {
+    alert("please enter a number betweern 8 - 128 charecters");
+    return;
   }
-  return array
+
+  let randInt = Math.floor(length/4);
+  var numbers = prompt("Click 'ok' to confirm the addition of numbers in your in your password");  
+  if (numbers === true) {
+    for(i=0; i < randInt; i++) {
+      pw.push(numbers[Math.floor(10*Math.random())]);
+      return;
+    }
+  }
+
+  var addLowerCase = prompt("Click 'ok' to confirm the addition of numbers in your in your password"); 
+  if (addLowerCase === true) { 
+    for(i=0; i < randInt; i++) {
+      pw.push(lowerCase[Math.floor(10*Math.random())]);
+      return;
+    }
+  }
+
+  var addUpperCase = prompt("Click 'ok' to confirm the addition of numbers in your in your password");  
+  if (addUpperCase === true) { 
+    for(i=0; i < randInt; i++) {
+      pw.push(upperCase[Math.floor(10*Math.random())]);
+      return;
+    }
+  }
+
+  var addSymbols = prompt("Click 'ok' to confirm the addition of numbers in your in your password");  
+  if (addSymbols === true) { 
+    for(i=0; i < randInt; i++) {
+      pw.push(symbols[Math.floor(10*Math.random())]);
+      return;
+    }
+  }
+
+  if(length !== pw) {
+    var fill = pw - length;
+    for (i=0; i < fill; i++) {
+      pw.push(1);    
+      return
+    }
+  }
 }
 
-function syncCharacterAmount(e) {
-  const value = e.target.value
-  characterAmountNumber.value = value
-  characterAmountRange.value = value
-}
